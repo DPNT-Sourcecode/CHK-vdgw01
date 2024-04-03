109f7a3cd1dad63a77375e30259b2d39037d8e13
sbx_user1051 <sbx_user1051@169.254.33.209>
2024-04-03 12:17:35 +0000

    Wed Apr 03 12:17:35 UTC 2024

--- test/solutions/CHK/test-checkout.js
+++ test/solutions/CHK/test-checkout.js
@@ -28,4 +28,12 @@ describe('CHK challenge R1: calculate checkout based on SKUs', function() {
 	it('should return 100 if passed SKU "AA"', function() {
 	    assert.equal(checkout("AA"), 100);
 	});
+
+	it('should return 100 if passed SKU "AB"', function() {
+	    assert.equal(checkout("AB"), 80);
+	});
+
+	it('should return 100 if passed SKU "AB"', function() {
+	    assert.equal(checkout("DD"), 30);
+	});
 })

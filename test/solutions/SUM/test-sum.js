2179871d1961871f2246adaf54b74aa6e9382dd4
sbx_user1051 <sbx_user1051@169.254.33.209>
2024-04-03 11:26:55 +0000

    Wed Apr 03 11:26:55 UTC 2024

--- test/solutions/SUM/test-sum.js
+++ test/solutions/SUM/test-sum.js
@@ -8,4 +8,12 @@ describe('SUM challenge: adding two numbers', function() {
 	it('should return 3, which is the sum of 1 and 2', function() {
 	    assert.equal(sum(1, 2), 3);
 	});
+
+	it('should return 4, which is the sum of 2 and 2', function() {
+	    assert.equal(sum(2, 2), 4);
+	});
+
+	it('should return 100, which is the sum of 0 and 100', function() {
+	    assert.equal(sum(0, 100), 100);
+	});
 });

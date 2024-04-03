a7ad9f75d08c2355aeb021a2f6af6f0275a0365d
sbx_user1051 <sbx_user1051@169.254.33.209>
2024-04-03 11:53:35 +0000

    Wed Apr 03 11:53:35 UTC 2024

--- test/solutions/CHK/test-checkout.js
+++ test/solutions/CHK/test-checkout.js
@@ -3,3 +3,13 @@ var describe = mocha.describe
 var it = mocha.it
 var assert = require('assert');
 const checkout = require('../../../lib/solutions/CHK/checkout');
+
+describe('CHK challenge R1: calculate checkout based on SKUs', function() {
+	it('should return 50 if passed SKU "A"', function() {
+	    assert.equal(checkout("A"), 50);
+	});
+
+	it('should return 30 if passed SKU "B"', function() {
+	    assert.equal(checkout("B"), 30);
+	});
+})

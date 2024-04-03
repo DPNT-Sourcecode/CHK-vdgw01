74737833fdf70e876d2de2e88728e3061bf9615a
sbx_user1051 <sbx_user1051@169.254.33.209>
2024-04-03 11:35:51 +0000

    Wed Apr 03 11:35:51 UTC 2024

--- test/solutions/HLO/test-hello.js
+++ test/solutions/HLO/test-hello.js
@@ -3,3 +3,9 @@ var describe = mocha.describe
 var it = mocha.it
 var assert = require('assert');
 const hello = require('../../../lib/solutions/HLO/hello');
+
+describe('HLO challenge: saying hello to the world', function() {
+	it('should say hello to the world', function() {
+	    assert.equal(hello(), "Hello, World!");
+	});
+})
